b0f92b4bfd13d5b586f83537910d8944ddeeed00
dungbk10t <dungbk10t@gmail.com>
2022-02-02 19:38:42 +0700

    Updates

---
+++ precache-manifest.dd09bf41d73626c3a4212cf0cedb33d5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a7e711b54d6f4fbdaf21bab7f5c7a1",
+    "url": "/tuandung_portfolio/index.html"
+  },
+  {
+    "revision": "8754ce9b085d3a23b40d",
+    "url": "/tuandung_portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "67a41ce975a447263e8c",
+    "url": "/tuandung_portfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "e908b93fe0e5ae9d1d15",
+    "url": "/tuandung_portfolio/static/css/main.1637c26d.chunk.css"
+  },
+  {
+    "revision": "e6d050331368197e3cbd",
+    "url": "/tuandung_portfolio/static/js/2.80eeee9b.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/tuandung_portfolio/static/js/2.80eeee9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8754ce9b085d3a23b40d",
+    "url": "/tuandung_portfolio/static/js/3.535b7d0f.chunk.js"
+  },
+  {
+    "revision": "67a41ce975a447263e8c",
+    "url": "/tuandung_portfolio/static/js/4.5a0358c2.chunk.js"
+  },
+  {
+    "revision": "e908b93fe0e5ae9d1d15",
+    "url": "/tuandung_portfolio/static/js/main.46830b91.chunk.js"
+  },
+  {
+    "revision": "ba020866e7ca6f1e21b2",
+    "url": "/tuandung_portfolio/static/js/runtime-main.8887281f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/tuandung_portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "5e05e0e070609d503cabd6ee9571418c",
+    "url": "/tuandung_portfolio/static/media/FPTLogo.5e05e0e0.png"
+  },
+  {
+    "revision": "3382fc3fba05e7cdf214f83a53732f44",
+    "url": "/tuandung_portfolio/static/media/HUSTLogo.3382fc3f.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/tuandung_portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b31eb1f99f6b3f15c69d0ccd49452e18",
+    "url": "/tuandung_portfolio/static/media/certi_Tokyo.b31eb1f9.png"
+  },
+  {
+    "revision": "af012e0d3956a68f68f8772054873a1c",
+    "url": "/tuandung_portfolio/static/media/certi_aws_cloud_practitioner.af012e0d.png"
+  },
+  {
+    "revision": "f5f11beab2cc566001f3aceaef58f247",
+    "url": "/tuandung_portfolio/static/media/certi_courseraLogo.f5f11bea.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/tuandung_portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/tuandung_portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/tuandung_portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "115515ce057691df4a7371b377efd26e",
+    "url": "/tuandung_portfolio/static/media/mindxLogo.115515ce.png"
+  },
+  {
+    "revision": "3725517f75e432dac46349044c207c07",
+    "url": "/tuandung_portfolio/static/media/misaLogo.3725517f.png"
+  },
+  {
+    "revision": "347aa20b12a45fd0ea1c694e41545291",
+    "url": "/tuandung_portfolio/static/media/pj_cardLogo.347aa20b.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/tuandung_portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/tuandung_portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
