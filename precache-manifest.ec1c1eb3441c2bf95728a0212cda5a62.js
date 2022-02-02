e6cb0d1452e5e50ef1c178978819b5ef972ddac6
dungbk10t <dungbk10t@gmail.com>
2022-02-02 19:29:52 +0700

    Updates

---
+++ precache-manifest.ec1c1eb3441c2bf95728a0212cda5a62.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb2c788fbf30dda7c85cfdc6b58825d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82e3c8bdf4fc91306f37",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "b4329ffccd20b39ff27e",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "6c30b12e989fc4a42882",
+    "url": "/static/css/main.361bf9f6.chunk.css"
+  },
+  {
+    "revision": "442e261b6a92b058d38f",
+    "url": "/static/js/2.ab0b7fc3.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.ab0b7fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e3c8bdf4fc91306f37",
+    "url": "/static/js/3.f026804a.chunk.js"
+  },
+  {
+    "revision": "b4329ffccd20b39ff27e",
+    "url": "/static/js/4.d58c9af6.chunk.js"
+  },
+  {
+    "revision": "6c30b12e989fc4a42882",
+    "url": "/static/js/main.648e3d1f.chunk.js"
+  },
+  {
+    "revision": "76ee2b28baa0d109a594",
+    "url": "/static/js/runtime-main.a6ac7869.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "5e05e0e070609d503cabd6ee9571418c",
+    "url": "/static/media/FPTLogo.5e05e0e0.png"
+  },
+  {
+    "revision": "3382fc3fba05e7cdf214f83a53732f44",
+    "url": "/static/media/HUSTLogo.3382fc3f.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b31eb1f99f6b3f15c69d0ccd49452e18",
+    "url": "/static/media/certi_Tokyo.b31eb1f9.png"
+  },
+  {
+    "revision": "af012e0d3956a68f68f8772054873a1c",
+    "url": "/static/media/certi_aws_cloud_practitioner.af012e0d.png"
+  },
+  {
+    "revision": "f5f11beab2cc566001f3aceaef58f247",
+    "url": "/static/media/certi_courseraLogo.f5f11bea.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "115515ce057691df4a7371b377efd26e",
+    "url": "/static/media/mindxLogo.115515ce.png"
+  },
+  {
+    "revision": "3725517f75e432dac46349044c207c07",
+    "url": "/static/media/misaLogo.3725517f.png"
+  },
+  {
+    "revision": "347aa20b12a45fd0ea1c694e41545291",
+    "url": "/static/media/pj_cardLogo.347aa20b.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
